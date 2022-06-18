327515dbd8dc600be07c7a0f49160297c66628c4
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-19 01:32:39 +0200

    Fetch api created

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url, {
+        headers: {
+            'X-RapidAPI-Key': '55c7aff6eemshcd383819be5fbf5p141342jsn345fe6b78f78',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+        }
+    }))
+
+}
