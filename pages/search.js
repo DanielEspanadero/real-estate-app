239bb0a542bc7bf45e50a3a82ca0c8b153398345
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-21 19:36:58 +0200

    SearchFilters created

--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
+import SearchFilters from '../components/SearchFilters';
+
 const Search = () => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
@@ -12,10 +14,22 @@ const Search = () => {
         <Box>
             <Flex
             cursor='pointer'
-            
+            bg='gray.100'
+            borderBottom='1px'
+            borderColor='gray.200'
+            p='2'
+            fontWeight='black'
+            fontSize='lg'
+            justifyContent='center'
+            alignItems='center'
+            onClick={()=> setSearchFilters((prevFilters)=> !prevFilters)}
             >
-
+                <Text>Search Property By Filters</Text>
+                <Icon paddingLeft='2' w='7' as={BsFilter} />
             </Flex>
+            {searchFilters && <searchFilters />}
         </Box>
     )
-}
+};
+
+export default Search;
