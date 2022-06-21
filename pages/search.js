34376f6e88b6a5762a09cc949cf9af4bbec16c90
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-21 19:58:58 +0200

    Starting width search route

--- pages/search.js
+++ pages/search.js
@@ -4,32 +4,57 @@ import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
+import Property from '../components/Properties';
 import SearchFilters from '../components/SearchFilters';
+import noresult from '../assets/images/noresult.svg'
 
-const Search = () => {
+const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
     return (
         <Box>
             <Flex
-            cursor='pointer'
-            bg='gray.100'
-            borderBottom='1px'
-            borderColor='gray.200'
-            p='2'
-            fontWeight='black'
-            fontSize='lg'
-            justifyContent='center'
-            alignItems='center'
-            onClick={()=> setSearchFilters((prevFilters)=> !prevFilters)}
+                cursor='pointer'
+                bg='gray.100'
+                borderBottom='1px'
+                borderColor='gray.200'
+                p='2'
+                fontWeight='black'
+                fontSize='lg'
+                justifyContent='center'
+                alignItems='center'
+                onClick={() => setSearchFilters((prevFilters) => !prevFilters)}
             >
                 <Text>Search Property By Filters</Text>
                 <Icon paddingLeft='2' w='7' as={BsFilter} />
             </Flex>
-            {searchFilters && <searchFilters />}
+            {searchFilters && <SearchFilters />}
+            <Text fontSize='2xl' p='4' fontWeight='bold'>
+                Properties {router.query.purpose}
+            </Text>
+            <Flex flexWrap='wrap'>
+                {properties.map((property) => <Property property={property} key={property.id} />)}
+            </Flex>
+            {properties.length === 0 && (
+                <Flex justifyContent='center' alignItems='center' flexDirection='column' marginTop='5' marginBottom='5'>
+                    <Image alt='no result' src={noresult} />
+                    <Text fontSize='2xl' marginTop='3'>No Results Found</Text>
+                </Flex>
+            )}
         </Box>
     )
+};
+
+export async function getStaticProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency
+
+    return {
+        props: {
+            propertiesForSale: propertyForSale?.hits
+        }
+    };
 };
 
 export default Search;
