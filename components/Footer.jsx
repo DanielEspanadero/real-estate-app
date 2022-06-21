83ee55cc0046698de1e3431451bdf8db34166184
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-21 18:55:19 +0200

    Footer created

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => (
+    <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+        Created by <a href='https://www.daniel-espanadero.com/' target='_BLANK'>Daniel Españadero</a> © {new Date().getFullYear()}
+    </Box>
+);
+
+export default Footer;
