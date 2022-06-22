cef2cb150c657608a9c75332556be91ad800ed3b
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-22 22:22:32 +0200

    Search options finished

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,8 @@ import { BsFilter } from 'react-icons/bs';
 
 import Property from '../components/Properties';
 import SearchFilters from '../components/SearchFilters';
-import noresult from '../assets/images/noresult.svg'
+import noresult from '../assets/images/noresult.svg';
+import { fetchApi, baseUrl } from '../utils/fetchApi';
 
 const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
@@ -46,13 +47,24 @@ const Search = ({ properties }) => {
     )
 };
 
-export async function getStaticProps({ query }) {
+export async function getServerSideProps({ query }) {
     const purpose = query.purpose || 'for-rent';
-    const rentFrequency = query.rentFrequency
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
 
     return {
         props: {
-            propertiesForSale: propertyForSale?.hits
+            properties: data?.hits
         }
     };
 };
