203c9435d4963fff4ed0f70a8d3a67fc667795d3
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-23 01:51:04 +0200

    Bugs fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 
 const Footer = () => (
     <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
-        Created by <a href='https://www.daniel-espanadero.com/' target='_BLANK'>Daniel Españadero</a> © {new Date().getFullYear()}
+        Created by <a href='https://www.daniel-espanadero.com/'>Daniel Españadero</a> © {new Date().getFullYear()}
     </Box>
 );
 
