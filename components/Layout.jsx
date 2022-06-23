12c96eaec9d73d43916d91379b5da206de98bf4d
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-23 02:17:50 +0200

    Title updated

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,10 @@ import Footer from './Footer';
 const Layout = ({ children }) => (
     <>
         <Head>
-            <title>Real Estate</title>
+            <title>Dubai Estate</title>
+            <meta charSet='utf-8' />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            {/* <link rel='shortcut icon' href='../assets/images/dubai-estate.icon' /> */}
         </Head>
         <Box maxWidth='1280px' m='auto'>
             <header>
